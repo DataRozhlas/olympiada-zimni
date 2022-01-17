e068df7710d3f86a8b95e2160651670eeaab4e42
Petr Kočí <petr.koci@samizdat.cz>
2022-01-17 17:29:04 +0100

    rozsekaná data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-﻿import "./byeie"; // loučíme se s IE
+﻿import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./byeie"; // loučíme se s IE
+
+ReactDOM.render(<App graf={"graf1"} />, document.getElementById("graf1"));
+ReactDOM.render(<App graf={"graf2"} />, document.getElementById("graf2"));
+ReactDOM.render(<App graf={"graf3"} />, document.getElementById("graf3"));
+ReactDOM.render(<App graf={"tabulka"} />, document.getElementById("tabulka"));
 
 /*
 // snadné načtení souboru pro každého!
---
+++ srv/dataPreparator.js
@@ -0,0 +1,57 @@
+import * as d3 from "d3-dsv";
+import fs from "fs";
+
+// načtení dat
+const data = d3.csvParse(fs.readFileSync("data/athlete_events.csv", "utf-8"));
+
+// výběr dat - zimní olympiáda, jen relevantní
+const winter = data
+  .filter((d) => d.Season === "Winter")
+  .map((d) => {
+    return {
+      n: d.Name,
+      s: d.Sex,
+      a: d.Age,
+      h: d.Height,
+      w: d.Weight,
+      t: d.Team,
+      y: d.Year,
+      c: d.City,
+      d: d.Sport,
+      m: d.Medal,
+    };
+  });
+
+// seznam roků a měst
+
+const roky = [...new Set(winter.map((d) => d.y))];
+
+const olympiady = roky.map((r, i) => {
+  return [Number(r), winter.filter((o) => o.y === r).map((d) => d.c)[0]];
+});
+
+fs.writeFileSync(
+  "data/olympiady.json",
+  JSON.stringify(olympiady.sort((a, b) => a[0] - b[0]))
+);
+
+// rozsekej data po letech
+
+roky.map((r) => {
+  const rok = winter
+    .filter((o) => o.y === r)
+    .map((a) => {
+      return {
+        n: a.n,
+        s: a.s,
+        a: a.a,
+        h: a.h,
+        w: a.w,
+        t: a.t,
+        d: a.d,
+        m: a.m,
+      };
+    });
+  const fileName = `data/${r}.json`;
+  fs.writeFileSync(fileName, JSON.stringify(rok));
+});
